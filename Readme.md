67aa2cfba990bdeef457e31337c869ad152e70b9
Sharadindu Das <remodas7774@gmail.com>
2024-04-26 13:27:07 +0530

    initializing the razorpay order

--- Readme.md
+++ Readme.md
@@ -14,3 +14,10 @@
 
 - Learn more about Cron Job [A cron job is a Linux command that schedules tasks to be executed in the future]
 
+- Order has 3 states: Created (User clicks on the Pay button), Attempted (User does the payment), Paid (User does the payment successfully)
+
+- Payment has 3 states: Created (User)
+
+- When someone buys a course and does payment how will I know that the money came to me or not? We will have to do verify using `webhook (kind of notification)`. When someone does the payment successfully, the `webhook` setup on `Razorpay website` will hit the API route provided by the Backend (StudyNotion) but how can we know that razorpay has done it or not ? we need some sort of verification right ? so we will use `secret key` that we will pass along with the `webhook` and in Backend (StudyNotion) we will verify that `secret key` using `verifySignature controller`
+
+- Capture payment means when the user clicks on the pay button on UI then a payment modal will open and ask for details and then order will be created and interaction between user's bank and razorpay account will be done and if successful, then pass the secret key along with the webhook and webhook will hit the API route (verify signature) and this will go to the StudyNotion Backend and there is also an secret key and both will be matched and if true then we can say that this is in Authorised State
--- backend/controllers/Payment.js
+++ backend/controllers/Payment.js
@@ -3,6 +3,7 @@ const Course = require("../models/Course")
 const User = require("../models/User")
 const {mailer} = require("../utils/mailer")
 const {courseEnrollment} = require("../mail/courseEnrollment")
+const mongoose = require("mongoose")
 
 // Capture the Payment and Initiate the Razorpay order
 exports.capturePayment = async (req,res) => {
@@ -12,32 +13,56 @@ exports.capturePayment = async (req,res) => {
         const userId = req.user.id
 
         // validation of the data
-        if(!courseId || !userId){
+        if(!courseId){
             return res.status(400).json({
                 success: false,
-                message: "All fields are required"
+                message: "Please provide the Course"
             })
         }
 
         // check if the course is valid or not
         const course = await Course.findById({_id: courseId})
         if(!course){
-            
+            return res.status(400).json({
+                success: false,
+                message: "Course is not found"
+            })
         }
 
-        // check if the user is valid or not
+        // check if the user already bought the course or not
+        const convertedUserId = new mongoose.Types.ObjectId(userId)
+        if(course.studentsEnrolled.includes(convertedUserId)){
+           return res.status(400).json({
+            success: false,
+            message: "Student is already enrolled to this course"
+           }) 
+        }
+
         // create an order
+        const amount = course.price
+        const currency = "INR"
+
+        var options = {
+            amount: amount * 100,
+            currency: currency,
+            receipt: Math.random(Date.now()).toString()
+        };
+
+        // initiate the payment using razorpay
+        const paymentResponse = instance.orders.create(options)
+        console.log(paymentResponse);
+
         // return the response
+        return res.status(200).json({
+            success: true,
+            paymentResponse
+        })
+
     } catch (err) {
         console.log(err.message);
         return res.status(500).json({
             success: false,
-            message: "Something went wrong while"
+            message: "Something went wrong while capturing the payment"
         })
     }
 }
-
-
-
-
-
