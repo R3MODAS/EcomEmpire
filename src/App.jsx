a4c43c9b7a74dcf3844fad4553f8c5d35a6a01ef
Sharadindu Das <remodas7774@gmail.com>
2024-05-03 20:54:54 +0530

    setting up things

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages"
+
 function App() {
   return (
-    <>
-     
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
